460da9862ac7edbca810c87f0b8df7ed9912c809
github@jldupont.com <github@jldupont.com>
2015-02-28 17:43:54 -0500

    step 1: input loop

--- src/jlddbus/script_signal.py
+++ src/jlddbus/script_signal.py
@@ -2,6 +2,8 @@
     Created on 2015-02-28
     @author: jldupont
 """
+import os
+import sys
 import logging
 import json
 
@@ -12,3 +14,25 @@ def run(*p, **k):
     Entry Point
     """
     
+    ppid=os.getppid()
+    logging.info("Process pid: %s" % os.getpid())
+    logging.info("Parent pid : %s" % ppid)
+    logging.info("Starting loop...")
+    
+    while True:
+        
+        ### protection against broken pipe
+        if os.getppid()!=ppid:
+            logging.warning("Parent process terminated... exiting")
+            break
+        
+        iline=sys.stdin.readline().strip()
+        
+        try:
+            jdict = json.loads(iline)
+        except:
+            logging.warning("Can't JSON decode: %s" % repr(iline))
+            continue
+        
+        
+        logging.info("Received: %s" % repr(jdict))
